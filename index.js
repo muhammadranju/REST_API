f233d4782d3692502dbd1a45c2b1dfbb0c578ccd
Muhammad Ranju <80270685+muhammadranju@users.noreply.github.com>
2022-05-07 15:37:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv");
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
    console.log(`server running at http://localhost:${PORT}`);
 });
